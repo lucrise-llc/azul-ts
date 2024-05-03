562c98af630d941113aaf31393973d12f0cd92d7
crisog <ortega.cpp@gmail.com>
2024-05-03 12:34:04 -0400

    feat: husky, prettier & lint-staged

--- src/azul-page.ts
+++ src/azul-page.ts
@@ -1,24 +1,24 @@
-import z from "zod";
-import crypto from "crypto";
+import z from 'zod';
+import crypto from 'crypto';
 
 export const PaymentRequesSchema = z
   .object({
-    currencyCode: z.enum(["$"]).default("$"),
+    currencyCode: z.enum(['$']).default('$'),
     orderNumber: z.string(),
     amount: z.number().int().positive(),
     ITBIS: z.number().int().positive(),
     approvedUrl: z.string().url(),
     declinedUrl: z.string().url(),
     cancelUrl: z.string().url(),
-    useCustomField1: z.enum(["1", "0"]).default("0"),
-    customField1Label: z.string().default("").optional(),
-    customField1Value: z.string().default("").optional(),
-    useCustomField2: z.enum(["1", "0"]).default("0"),
-    customField2Label: z.string().default("").optional(),
-    customField2Value: z.string().default("").optional(),
-    showTransactionResult: z.enum(["1", "0"]).default("1").optional(),
-    locale: z.enum(["ES", "EN"]).default("ES").optional(),
-    saveToDataVault: z.enum(["1", "0"]).default("0").optional(),
+    useCustomField1: z.enum(['1', '0']).default('0'),
+    customField1Label: z.string().default('').optional(),
+    customField1Value: z.string().default('').optional(),
+    useCustomField2: z.enum(['1', '0']).default('0'),
+    customField2Label: z.string().default('').optional(),
+    customField2Value: z.string().default('').optional(),
+    showTransactionResult: z.enum(['1', '0']).default('1').optional(),
+    locale: z.enum(['ES', 'EN']).default('ES').optional(),
+    saveToDataVault: z.enum(['1', '0']).default('0').optional(),
     dataVaultToken: z.string().optional(),
     altMerchantName: z
       .string()
@@ -30,42 +30,42 @@ export const PaymentRequesSchema = z
         }
         // This should only contain letters, numbers, spaces, and the following characters: . ,
         return /^[a-zA-Z0-9\s.,]*$/.test(value);
-      }),
+      })
   })
   .refine(
     (data) => {
-      return !(data.useCustomField1 === "1" && data.customField1Label === "");
+      return !(data.useCustomField1 === '1' && data.customField1Label === '');
     },
     {
-      message: "Custom field label is required when using custom fields",
-      path: ["customField1Label"],
+      message: 'Custom field label is required when using custom fields',
+      path: ['customField1Label']
     }
   )
   .refine(
     (data) => {
-      return !(data.useCustomField1 === "1" && data.customField1Value === "");
+      return !(data.useCustomField1 === '1' && data.customField1Value === '');
     },
     {
-      message: "Custom field value is required when using custom fields",
-      path: ["customField1Value"],
+      message: 'Custom field value is required when using custom fields',
+      path: ['customField1Value']
     }
   )
   .refine(
     (data) => {
-      return !(data.useCustomField2 === "1" && data.customField2Label === "");
+      return !(data.useCustomField2 === '1' && data.customField2Label === '');
     },
     {
-      message: "Custom field label is required when using custom fields",
-      path: ["customField2Label"],
+      message: 'Custom field label is required when using custom fields',
+      path: ['customField2Label']
     }
   )
   .refine(
     (data) => {
-      return !(data.useCustomField2 === "1" && data.customField2Value === "");
+      return !(data.useCustomField2 === '1' && data.customField2Value === '');
     },
     {
-      message: "Custom field value is required when using custom fields",
-      path: ["customField2Value"],
+      message: 'Custom field value is required when using custom fields',
+      path: ['customField2Value']
     }
   );
 
@@ -77,7 +77,7 @@ type AzulPageConfig = {
   merchantName: string;
   merchantType: string;
   authKey: string;
-  environment: "dev" | "prod";
+  environment: 'dev' | 'prod';
 };
 
 class AzulPage {
@@ -87,10 +87,10 @@ class AzulPage {
   constructor(config: AzulPageConfig) {
     this.config = config;
 
-    if (config.environment === "dev") {
-      this.azulURL = "https://pruebas.azul.com.do/PaymentPage/";
+    if (config.environment === 'dev') {
+      this.azulURL = 'https://pruebas.azul.com.do/PaymentPage/';
     } else {
-      this.azulURL = "https://pagos.azul.com.do/PaymentPage/";
+      this.azulURL = 'https://pagos.azul.com.do/PaymentPage/';
     }
   }
 
@@ -116,13 +116,10 @@ class AzulPage {
       paymentPageRequest.customField2Label,
       paymentPageRequest.customField2Value,
 
-      this.config.authKey,
-    ].join("");
+      this.config.authKey
+    ].join('');
 
-    return crypto
-      .createHmac("sha512", this.config.authKey)
-      .update(request)
-      .digest("hex");
+    return crypto.createHmac('sha512', this.config.authKey).update(request).digest('hex');
   }
 
   createForm(userRequest: PaymentPageRequestInput): string {
