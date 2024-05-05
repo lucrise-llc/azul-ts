ec087c74f8c2906a7917100d8af04a13ccfcea50
Luis Herasme <luis.alberto.herasme.cuevas@gmail.com>
2024-05-04 20:15:36 -0400

    Configures tests

---
+++ src/test/index.test.ts
@@ -0,0 +1,24 @@
+import AzulAPI from '../azul-api/api';
+import 'dotenv/config';
+
+const azul = new AzulAPI({
+  auth1: process.env.AUTH1!,
+  auth2: process.env.AUTH2!,
+  merchantId: process.env.MERCHANT_ID!,
+  certificatePath: process.env.CERTIFICATE_PATH!,
+  keyPath: process.env.KEY_PATH!
+});
+
+test('Can make a payment', async () => {
+  const result = await azul.payments.sale({
+    cardNumber: '6011000990099818',
+    expiration: '202412',
+    CVC: '818',
+    customOrderId: '1234',
+    amount: 1000,
+    ITBIS: 100
+  });
+
+  expect(result).toBeDefined();
+  expect(result.IsoCode).toBe('00');
+});
