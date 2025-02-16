67d28e7953d18f5e772808561dbe8814ad4d04ce
crisog <ortega.cpp@gmail.com>
2025-02-15 22:50:26 -0400

    fix(tests): adds retries to e2e test

--- tests/3ds.spec.ts
+++ tests/3ds.spec.ts
@@ -1,29 +1,33 @@
 import { test } from '@playwright/test';
 
-test('No challenge and no 3DS method', async ({ page }) => {
-  await page.goto('http://localhost:3000');
-  await page.click(`a[id=NO_CHALLENGE_NO_3DS_METHOD]`);
-  await page.waitForSelector('text=Payment successful');
-});
+test.describe('3DS Payment Tests', () => {
+  test.describe.configure({ retries: 3 });
 
-test('No challenge and 3DS method', async ({ page }) => {
-  await page.goto('http://localhost:3000');
-  await page.click(`a[id=NO_CHALLENGE_3DS_METHOD]`);
-  await page.waitForSelector('text=Payment successful');
-});
+  test('No challenge and no 3DS method', async ({ page }) => {
+    await page.goto('http://localhost:3000');
+    await page.click(`a[id=NO_CHALLENGE_NO_3DS_METHOD]`);
+    await page.waitForSelector('text=Payment successful');
+  });
 
-test('Challenge and no 3DS method', async ({ page }) => {
-  await page.goto('http://localhost:3000');
-  await page.click(`a[id=CHALLENGE_NO_3DS_METHOD]`);
-  await page.waitForSelector('#authform');
-  await page.click('#yes');
-  await page.waitForSelector('text=Payment successful');
-});
+  test('No challenge and 3DS method', async ({ page }) => {
+    await page.goto('http://localhost:3000');
+    await page.click(`a[id=NO_CHALLENGE_3DS_METHOD]`);
+    await page.waitForSelector('text=Payment successful');
+  });
 
-test('Challenge and 3DS method', async ({ page }) => {
-  await page.goto('http://localhost:3000');
-  await page.click(`a[id=CHALLENGE_3DS_METHOD]`);
-  await page.waitForSelector('#authform');
-  await page.click('#yes');
-  await page.waitForSelector('text=Payment successful');
+  test('Challenge and no 3DS method', async ({ page }) => {
+    await page.goto('http://localhost:3000');
+    await page.click(`a[id=CHALLENGE_NO_3DS_METHOD]`);
+    await page.waitForSelector('#authform');
+    await page.click('#yes');
+    await page.waitForSelector('text=Payment successful');
+  });
+
+  test('Challenge and 3DS method', async ({ page }) => {
+    await page.goto('http://localhost:3000');
+    await page.click(`a[id=CHALLENGE_3DS_METHOD]`);
+    await page.waitForSelector('#authform');
+    await page.click('#yes');
+    await page.waitForSelector('text=Payment successful');
+  });
 });
