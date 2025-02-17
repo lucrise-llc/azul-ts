d84b1a5174f042b05e949a09c6cb94dac421adc5
Luis Herasme <luis.alberto.herasme.cuevas@gmail.com>
2025-02-17 18:21:21 -0400

    feat: add simple example for Azul payment processing and remove outdated payment example

---
+++ examples/simple-example.ts
@@ -0,0 +1,27 @@
+import { Azul } from '../src/api';
+import { env } from '../src/tests/instance';
+
+const azul = new Azul({
+  auth1: env.AUTH1,
+  auth2: env.AUTH2,
+  merchantId: env.MERCHANT_ID,
+  certificate: env.AZUL_CERT,
+  key: env.AZUL_KEY
+});
+
+const result = await azul.sale({
+  type: 'card',
+  cardNumber: '6011000990099818',
+  expiration: '202412',
+  CVC: '818',
+  amount: 1000,
+  ITBIS: 100
+});
+
+if (result.type === 'success') {
+  console.log('Success');
+}
+
+if (result.type === 'error') {
+  console.error(`Payment failed: ${result.ErrorDescription}`);
+}
